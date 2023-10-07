f1b191f62f069a41cf5df58313fe349384cc9727
DOdGe <dimacernovolov@gmail.com>
2023-10-08 02:01:30 +0300

    done

---
+++ Par_5.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1_Part_3
+{
+    internal class Par_5
+    {
+        string text;
+        public Par_5()
+        {
+            this.text = "Я сошью себе черные штаны\nиз бархата голоса моего.\nЖелтую кофту из трех аршин заката.\nПо Невскому мира, по лощеным полосам его,\nпрофланирую шагом Дон-Жуана и фата.";
+        }
+        public Par_5(string _text)
+        {
+            this.text = _text;
+        }
+        public void Start()
+        {
+            int countWords = 0;
+            string cleanedText = RemoveNonAlphabeticCharacters(text);
+            var words = new SortedDictionary<char, int>();
+            char tempKey;
+            int tempValue;
+            bool isBeen = false;
+            string tempText = "";
+            foreach (char c in cleanedText)
+                tempText += char.ToLower(c);
+            for (int i = 0; i < tempText.Length - 1; i++)
+            {
+                foreach (var word in words)
+                {
+                    isBeen = tempText[i] == word.Key ? true : false;
+                    if (isBeen) break;
+                }
+                if (isBeen)
+                {
+                    continue;
+                }
+                else
+                {
+                    ++countWords;
+                    tempKey = tempText[i];
+                    tempValue = 1;
+                    for (int j = i + 1; j < tempText.Length; j++)
+                        if (tempText[i] == tempText[j])
+                            ++tempValue;
+                    words.Add(tempKey, tempValue);
+                }
+            }
+            Console.WriteLine($"Исходный текст:\n{text}\n");
+            Console.WriteLine($"Количество букв: {countWords}");
+            Console.WriteLine("Сводка:");
+            int tempCount = 0;
+            foreach (var word in words) 
+            {
+                if (tempCount == 7)
+                {
+                    Console.WriteLine();
+                    tempCount = 1;
+                }
+                else
+                {
+                    ++tempCount;
+                }
+                Console.Write($"{word.Key}({word.Value})\t");
+            }
+            Console.WriteLine("\n");
+        }
+        string RemoveNonAlphabeticCharacters(string text)
+        {
+            StringBuilder cleanedText = new StringBuilder();
+            foreach (char c in text)
+                if (Char.IsLetter(c))
+                    cleanedText.Append(c);
+            return cleanedText.ToString();
+        }
+    }
+}
---
+++ Par_6.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1_Part_3
+{
+    internal class Par_6
+    {
+        string word = "санки";
+        char[,] collection = 
+        { 
+            { 'ф', 'е', 'и' },
+            { 'м', 'о', 'с' },
+            { 'г', 'ч', 'п' },
+            { 'о', 'к', 'е' },
+            { 'р', 'б', 'я' },
+            { 'ю', 'н', 'ы' },
+            { 'я', 'а', 'ж' },
+            { 'е', 'э', 'о' },
+        };
+        public void Start()
+        {
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < 8; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                    Console.Write($"{collection[i, j]}  ");
+                Console.WriteLine();
+            }
+            Console.WriteLine($"\nСлово: {word}\n");
+            var list = collection.Cast<char>().ToList();
+            bool canDo()
+            {
+                if (word.Length > list.Count()) return false;
+                foreach (var w in word)
+                    if (!list.Remove(w)) return false;
+                return true;
+            }
+            Console.Write($"Составить слово ");
+            if (canDo())
+            {
+                Console.WriteLine("возможно");
+            }
+            else
+            {
+                Console.WriteLine("невозможно");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
